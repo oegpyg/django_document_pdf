d708e2ffa183cbe7a9d1dddc3ba906f33aaf57e9
oegpyg <oegpyg@gmail.com>
2024-02-06 14:37:17 -0300

    Create initials models

--- README.md
+++ README.md
@@ -14,3 +14,37 @@ It has only been tested in Django 4.0 and higher. For older versions of Django, 
 If you would like to see something added, please request it! As I said, this is still in an early stage of development and I'm open to all suggestions.
 
 If you find a bug, please report it!
+
+## Installation
+
+You will need to add `django_document_pdf` to your `INSTALLED_APPS` in your `settings.py` file:
+
+```python
+INSTALLED_APPS = [
+    ...
+    'django_document_pdf',
+    ...
+]
+```
+
+You will also need to add the following to your `urls.py` file:
+
+```python
+from django.urls import path, include
+
+urlpatterns = [
+    ...
+    path('django_document_pdf/', include('django_document_pdf.urls')),
+    ...
+]
+```
+
+## Usage
+
+Create **_fonts_** and **_images_** folder inside the MEDIA path.
+
+Make sure to include `MEDIA_URL` and `MEDIA_ROOT` settings in your project's `settings.py` file:
+```python
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+```
--- models.py
+++ models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class PDFFont(models.Model):
+    Code = models.CharField(max_length=15, unique=True)
+    Font = models.FileField(upload_to='fonts/')
+
+
+class FontStyle(models.Model):
+    Code = models.CharField(max_length=15, unique=True)
+    PDFFont = models.ForeignKey(PDFFont, on_delete=models.PROTECT)
+    Color = models.CharField(
+        max_length=15, default="black", null=False, blank=False)
+    Size = models.SmallIntegerField(default=12, null=False, blank=False)
+
+    def clean(self):
+        if self.Code:
+            existing = FontStyle.objects.filter(
+                Code=self.Code).exclude(pk=self.pk)
+            if existing.exists():
+                raise ValidationError("The FontStyle already exists.")
+        super(FontStyle, self).clean()
+
+    def __str__(self):
+        return self.Code
+
+
+class DocumentSpec(models.Model):
+    Code = models.CharField(max_length=30, unique=True)
+    Height = models.BigIntegerField(null=True)
+    Width = models.BigIntegerField(null=True)
+    ScreenDpi = models.BigIntegerField(null=True)
+    RowsPerPage = models.BigIntegerField(null=True)
+
+    def clean(self):
+        if self.Code:
+            existing = DocumentSpec.objects.filter(
+                Code=self.Code).exclude(pk=self.pk)
+            if existing.exists():
+                raise ValidationError(
+                    "The Document Specification already exists.")
+        super(DocumentSpec, self).clean()
+
+    def __str__(self):
+        return self.Code
+
+
+class DocumentSpecFields(models.Model):
+    DocumentSpec = models.ForeignKey(DocumentSpec, on_delete=models.PROTECT)
+    Field = models.CharField(max_length=50)
+    Style = models.ForeignKey(FontStyle)
+    types = ((0, 'Header'), (1, 'Detail'))
+    Type = models.SmallIntegerField(choices=types)
+    Width = models.IntegerField(null=True)
+    Y = models.IntegerField(null=False, blank=False)
+    X = models.IntegerField(null=False, blank=False)
+    Decimals = models.IntegerField(default=0)
+    _align = ((0, 'Left'), (1, 'Centered'), (2, 'Right'))
+    Alignment = models.SmallIntegerField(choices=_align)
+    TextLimit = models.IntegerField(null=True)
+
+
+class DocumentSpecLabels(models.Model):
+    DocumentSpec = models.ForeignKey(DocumentSpec, on_delete=models.PROTECT)
+    Text = models.CharField(max_length=255, blank=True)
+    Style = models.ForeignKey(FontStyle)
+    Y = models.IntegerField(null=False, blank=False)
+    X = models.IntegerField(null=False, blank=False)
+    _align = ((0, 'Left'), (1, 'Centered'), (2, 'Right'))
+    Alignment = models.SmallIntegerField(choices=_align)
+
+
+class DocumentSpecRects(models.Model):
+    DocumentSpec = models.ForeignKey(DocumentSpec, on_delete=models.PROTECT)
+    Height = models.IntegerField(null=False, blank=False)
+    Width = models.IntegerField(null=False, blank=False)
+    Y = models.IntegerField(null=False, blank=False)
+    X = models.IntegerField(null=False, blank=False)
+
+
+class DocumentSpecImages(models.Model):
+    DocumentSpec = models.ForeignKey(DocumentSpec, on_delete=models.PROTECT)
+    Height = models.IntegerField(null=False, blank=False)
+    Width = models.IntegerField(null=False, blank=False)
+    Y = models.IntegerField(null=False, blank=False)
+    X = models.IntegerField(null=False, blank=False)
+    Filename = models.FileField(upload_to='images/')
